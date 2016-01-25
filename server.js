a8e4992606e903d6564e295587f418e2afeb2aaf
Jeff Wakeman <Deftwun@gmail.com>
2016-01-25 12:12:40 -0500

    All is working

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 "use strict";
 
 var express = require("express"),
-    app = express();
+    app = express(),
+    multer = require("multer");
   
 var port = process.env.PORT || 8080;
+
+app.use("/",express.static("client"));
+
+app.post('/upload', multer({}).single('file-input'), function(req,res){
+    res.json({size:req.file.size});
+});
 
 app.listen(port, function () {
   console.log('App running on port # ' + port);
